8982b8f41c36bb14653df455004bcca7a9d31f03
Tina Fredericks <tina.k.fredericks@gmail.com>
2016-01-20 10:31:11 -0800

    change HttpPost to HttpGet

--- app/src/main/java/com/tinakit/android/miniyelp/BusinessListFragment.java
+++ app/src/main/java/com/tinakit/android/miniyelp/BusinessListFragment.java
@@ -30,6 +30,7 @@ import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
@@ -40,6 +41,8 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 /**
@@ -201,14 +204,22 @@ public class BusinessListFragment extends ListFragment{
 
             DefaultHttpClient httpClient = new DefaultHttpClient(new BasicHttpParams());
             //access web service data tables
-            HttpPost httppost = new HttpPost(buildYahooBizUrl());
-            httppost.setHeader("Content-type","application/json");
+            HttpGet httpGet = new HttpGet();
+            httpGet.setHeader("Content-type","application/json");
+            try{
+                httpGet.setURI(new URI(buildYahooBizUrl()));
+            }catch (URISyntaxException use){
+
+                use.printStackTrace();
+            }
+
+
             InputStream inputStream = null;
 
             String result = null;
 
             try{
-                HttpResponse response = httpClient.execute(httppost);
+                HttpResponse response = httpClient.execute(httpGet);
                 HttpEntity entity = response.getEntity();
                 inputStream = entity.getContent();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
